61bc243f2a6be4e2e25c1af88f75c263b2be26f2
Thong Tuan Kiet <thongtuankiet.1999@gmail.com>
2021-06-26 20:32:40 +0700

    getters for test

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <button @click="tang">Tang</button>
+    <button @click="giam">Giam</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+
+    }
+  },
+  methods: {
+    tang(){
+      return this.$store.state.result++; 
+    },
+    giam(){
+      return this.$store.state.result--;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/OtherResult.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="result">
+        <h2>This is other result: {{ result }} </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        result() {
+            return this.$store.getters.doubleNumber;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
