28e3075c2ee92f3100eabd8747605ee4aa5c73fe
Thong Tuan Kiet <thongtuankiet.1999@gmail.com>
2021-06-28 19:36:09 +0700

    test scss

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
-    <button @click="callIncrementMutation">Tang</button>
-    <button @click="tryAsync">Giam</button>
+    <div class="title">
+      <h2>Please click any button to see what happens</h2>
+    </div>
+    <button class="btn btn-incre" @click="callIncrementMutation">Tang</button>
+    <button class="btn btn-decre" @click="tryAsync">Giam</button>
   </div>
 </template>
 
@@ -22,5 +25,25 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "@/assets/_shared";
+.hello{
+  .title{
+    text-transform: $textUper;
+  }
+  .btn{
+    cursor: pointer;
+    padding: $paddingBtn;
+    margin: 20px;
+    border: none;
+    color: #fff;
+  }
+  .btn-incre{
+    background-color: $backgroundBtnSuccess;
+  }
+  .btn-decre{
+    background-color: $backgroundBtnDanger;
+  }
+}
+
 </style>
